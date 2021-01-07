9570075285571b33672a3483b04b32214fa20af4
boniqa <budnik.wer@gmail.com>
2021-01-07 18:19:07 +0100

    display raw data

--- geo-api/src/app/areas/main/main.component.ts
+++ geo-api/src/app/areas/main/main.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { GeoApiService } from '../../shared/services/geo-api.service';
 
 @Component({
   selector: 'app-main',
@@ -7,9 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  loaded: boolean;
+  items: any;
+
+  constructor(private geoApiService: GeoApiService) { }
 
   ngOnInit(): void {
+    this.loaded = false;
+    this.geoApiService.getData().subscribe((result)=> {
+      this.items = result.containeditems;
+      console.log(this.items);
+      this.loaded = true;
+    })
+  }
+
+  getStatus(status){
+    return ( status === 'Gyldig' ? 'valid' : 'retired');
   }
 
 }
